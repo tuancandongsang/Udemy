4d402165873354834622c13ca0754a727e64d2b3
tuancan <tuacandongsang@gmail.com>
2022-11-29 00:02:11 +0700

    29.11

--- Udemy router/VueRouter/src/components/users/UsersList.vue
+++ Udemy router/VueRouter/src/components/users/UsersList.vue
@@ -1,14 +1,18 @@
 <template>
-  <button @click="confirmInput">Confirm</button>
-  <button @click="saveChanges">Save Changes</button>
-  <ul>
-    <user-item
-      v-for="user in users"
-      :key="user.id"
-      :name="user.fullName"
-      :role="user.role"
-    ></user-item>
-  </ul>
+  <div class="container">
+    <div class="form">
+      <label for="">Seach</label>
+      <input type="text" v-model="search" placeholder="search for fullName" />
+    </div>
+    <ul>
+      <user-item
+        v-for="user in users"
+        :key="user.id"
+        :name="user.fullName"
+        :role="user.role"
+      ></user-item>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -18,28 +22,75 @@ export default {
   components: {
     UserItem,
   },
-  inject: ['users'],
+  // inject: ['users'],
   data() {
-    return { changesSaved: false };
+    return {
+      search: this.$route.query.search,
+      users: [
+        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
+        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
+        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
+        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
+      ],
+    };
   },
-  beforeRouteEnter(to, from, next) {
-    next(false);
-  },
-  methods: {
-    confirmInput() {
-      this.$router.push('/teams');
+  methods: {},
+  computeds: {
+    users() {
+      if (this.search) {
+        return this.users.filter((user) =>
+          user.fullName.toLowerCase().includes(this.search.toLowerCase())
+        );
+      } else {
+        return this.users;
+      }
     },
-    saveChanges() {
-      this.changesSaved = true;
+  },
+  watch: {
+    search(value, _) {
+      console.log(_);
+      if (value) {
+        this.$router.push({
+          query: {
+            search: this.search.trim(),
+          },
+        });
+        console.log(this.$route.query.search);
+      }
     },
   },
-  unmounted() {
-    // console.log('unmounted');
+  created() {
+    console.log(this.users);
+    this.$watch(
+      () => this.$route.query.search,
+      (value, _) => {
+        this.search = value;
+        console.log(_);
+      }
+    );
   },
+  // beforeRouteEnter(to, from, next) {
+  //   next(false);
+  // },
 };
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.form {
+  background-color: aquamarine;
+  margin: 20px;
+}
+.form > input {
+  outline: none;
+  padding: 4px 8px;
+  font-size: 1.5rem;
+}
 ul {
   list-style: none;
   margin: 2rem auto;
