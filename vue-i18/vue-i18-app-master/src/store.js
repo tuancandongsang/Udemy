39922d4cbc205af991bea6ef96d04b2d050da495
tuancan <tuacandongsang@gmail.com>
2023-01-29 23:47:24 +0700

    29.1

--- vue-i18/vue-i18-app-master/src/store.js
+++ vue-i18/vue-i18-app-master/src/store.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import app from './main'
+import Vue from "vue";
+import Vuex from "vuex";
+import app from "./main";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.store({
-  const mutations = {
-    SET_LANG (state, payload) {
-      app.$i18n.locale = payload
-    }
+  mutations: {
+    SET_LANG(state, payload) {
+      app.$i18n.locale = payload;
+    },
   },
-  
-  const actions = {
-    setLang({commit}, payload) {
-      commit('SET_LANG', payload)
-    }
-  }
-})
+
+  actions: {
+    setLang({ commit }, payload) {
+      commit("SET_LANG", payload);
+    },
+  },
+});
