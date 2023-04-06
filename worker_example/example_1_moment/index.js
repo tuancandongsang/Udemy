618c5dc65fe3340cdd773aa5d6e4869bbe3ce3b7
tuancan <tuacandongsang@gmail.com>
2023-04-07 00:04:33 +0700

    a

--- worker_example/example_1_moment/index.js
+++ worker_example/example_1_moment/index.js
@@ -1,22 +1,26 @@
-if ('Worker' in window) {
-    const worker = new Worker('worker.js');
-    worker.onmessage = function (event) {
-        const message = event.data;
-        switch (message.type) {
-            case 'UPDATE_TIME': {
-                const timeDOM = document.getElementById('time');
-                timeDOM.innerHTML = message.payload
-            }
-        }
-
-        const nameDOM = document.getElementById('name');
-        nameDOM.innerHTML = message.payload
+if ("Worker" in window) {
+  const worker = new Worker("worker.js");
+  worker.onmessage = function (event) {
+    const message = event.data;
+    switch (message.type) {
+      case "UPDATE_TIME": {
+        const timeDOM = document.getElementById("time");
+        timeDOM.innerHTML = message.payload;
+      }
     }
-    setInterval(() => {
-        worker.postMessage({
-            type: 'GET_NOW'
-        })
-    }, 1000)
+
+    const nameDOM = document.getElementById("name");
+    nameDOM.innerHTML = message.payload;
+
+    const callApi = document.getElementById("json");
+    callApi.innerText = event.data;
+    // console.log(event.data);
+  };
+  setInterval(() => {
+    worker.postMessage({
+      type: "GET_NOW",
+    });
+  }, 1000);
 } else {
-    console.log('not Support Worker')
+  console.log("not Support Worker");
 }
--- worker_example/example_1_moment/worker.js
+++ worker_example/example_1_moment/worker.js
@@ -1,19 +1,30 @@
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js')
-self.onmessage = function (event) {
-    const message = event.data;
-    switch (message.type) {
-        case 'GET_NOW': {
-            sendMessage({
-                type: 'UPDATE_TIME',
-                payload: moment().format('DD/MM/YYYY hh:mm:ss')
+importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"
+);
+var xhr = new XMLHttpRequest();
+xhr.onload = function () {
+  // Nếu nội dung được tải về thành công, trả lại nội dung cho trang web
+  if (xhr.status === 200) {
+    postMessage(xhr.responseText);
+  }
+};
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+xhr.send();
 
-            })
-        }
+self.onmessage = function (event) {
+  const message = event.data;
+  switch (message.type) {
+    case "GET_NOW": {
+      sendMessage({
+        type: "UPDATE_TIME",
+        payload: moment().format("DD/MM/YYYY hh:mm:ss"),
+      });
     }
-    sendMessage({
-        payload: "tuancandongsang"
-    })
-}
+  }
+  sendMessage({
+    payload: "tuancandongsang",
+  });
+};
 function sendMessage(message) {
-    return self.postMessage(message)
+  return self.postMessage(message);
 }
